9dd5053ce540fc995f400595e42ac5ebf50e1e51
ArtMan-8 <ya.Artriches@ya.ru>
2021-12-16 19:22:39 +0300

    add: serverless-finch

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -11,91 +11,91 @@ import Cart from "components/MainLayout/components/Cart";
 import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
-    homeLink: {
-      color: "white",
-      textDecoration: "none",
-    },
-  })
+	createStyles({
+		root: {
+			flexGrow: 1,
+		},
+		menuButton: {
+			marginRight: theme.spacing(2),
+		},
+		title: {
+			flexGrow: 1,
+		},
+		homeLink: {
+			color: "white",
+			textDecoration: "none",
+		},
+	})
 );
 
 export default function Header() {
-  const classes = useStyles();
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
-  const auth = true;
+	const classes = useStyles();
+	const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+	const open = Boolean(anchorEl);
+	const auth = true;
 
-  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
+	const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+		setAnchorEl(event.currentTarget);
+	};
 
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
 
-  return (
-    <AppBar position="relative">
-      <Toolbar>
-        <Typography variant="h6" className={classes.title}>
-          <Link className={classes.homeLink} to="/">
-            Books Store
-          </Link>
-        </Typography>
+	return (
+		<AppBar position="relative">
+			<Toolbar>
+				<Typography variant="h6" className={classes.title}>
+					<Link className={classes.homeLink} to="/">
+						Books Store
+					</Link>
+				</Typography>
 
-        {auth && (
-          <div>
-            <IconButton
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleMenu}
-              color="inherit"
-            >
-              <AccountCircle />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorEl}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={open}
-              onClose={handleClose}
-            >
-              <MenuItem
-                component={Link}
-                to="/admin/orders"
-                onClick={handleClose}
-              >
-                Manage orders
-              </MenuItem>
-              <MenuItem
-                component={Link}
-                to="/admin/products"
-                onClick={handleClose}
-              >
-                Manage products
-              </MenuItem>
-            </Menu>
-          </div>
-        )}
-        <Cart />
-      </Toolbar>
-    </AppBar>
-  );
+				{auth && (
+					<div>
+						<IconButton
+							aria-label="account of current user"
+							aria-controls="menu-appbar"
+							aria-haspopup="true"
+							onClick={handleMenu}
+							color="inherit"
+						>
+							<AccountCircle />
+						</IconButton>
+						<Menu
+							id="menu-appbar"
+							anchorEl={anchorEl}
+							anchorOrigin={{
+								vertical: "top",
+								horizontal: "right",
+							}}
+							keepMounted
+							transformOrigin={{
+								vertical: "top",
+								horizontal: "right",
+							}}
+							open={open}
+							onClose={handleClose}
+						>
+							<MenuItem
+								component={Link}
+								to="/admin/orders"
+								onClick={handleClose}
+							>
+								Manage orders
+							</MenuItem>
+							<MenuItem
+								component={Link}
+								to="/admin/products"
+								onClick={handleClose}
+							>
+								Manage products
+							</MenuItem>
+						</Menu>
+					</div>
+				)}
+				<Cart />
+			</Toolbar>
+		</AppBar>
+	);
 }
