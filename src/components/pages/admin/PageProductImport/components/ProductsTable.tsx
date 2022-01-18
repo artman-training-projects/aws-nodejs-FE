30b9ab2415130d13bb1ee03b46542f0258814901
ArtMan-8 <ya.Artriches@ya.ru>
2022-01-18 21:59:32 +0300

    update: links

--- src/components/pages/admin/PageProductImport/components/ProductsTable.tsx
+++ src/components/pages/admin/PageProductImport/components/ProductsTable.tsx
@@ -13,77 +13,77 @@ import Button from "@material-ui/core/Button";
 import { formatAsPrice } from "utils/utils";
 
 export default function ProductsTable() {
-  const [products, setProducts] = useState<any>([]);
+	const [products, setProducts] = useState<any>([]);
 
-  useEffect(() => {
-    axios
-      .get(`${API_PATHS.bff}/product`)
-      .then((res) => setProducts(res.data))
-      .catch((e) => {
-        console.error(e);
-      });
-  }, []);
+	useEffect(() => {
+		axios
+			.get(`${API_PATHS.bff}/products`)
+			.then((res) => setProducts(res.data))
+			.catch((e) => {
+				console.error(e);
+			});
+	}, []);
 
-  const onDelete = (id: string) => {
-    axios
-      .delete(`${API_PATHS.bff}/product/${id}`)
-      .then(() => {
-        axios
-          .get(`${API_PATHS.bff}/product`)
-          .then((res) => setProducts(res.data))
-          .catch((e) => {
-            console.error(e);
-          });
-      })
-      .catch((e) => {
-        console.error(e);
-      });
-  };
+	const onDelete = (id: string) => {
+		axios
+			.delete(`${API_PATHS.bff}/product/${id}`)
+			.then(() => {
+				axios
+					.get(`${API_PATHS.bff}/products`)
+					.then((res) => setProducts(res.data))
+					.catch((e) => {
+						console.error(e);
+					});
+			})
+			.catch((e) => {
+				console.error(e);
+			});
+	};
 
-  return (
-    <TableContainer component={Paper}>
-      <Table aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Title</TableCell>
-            <TableCell align="right">Description</TableCell>
-            <TableCell align="right">Price</TableCell>
-            <TableCell align="right">Count</TableCell>
-            <TableCell align="right">Action</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {products.map((product: any) => (
-            <TableRow key={product.id}>
-              <TableCell component="th" scope="row">
-                {product.title}
-              </TableCell>
-              <TableCell align="right">{product.description}</TableCell>
-              <TableCell align="right">
-                {formatAsPrice(product.price)}
-              </TableCell>
-              <TableCell align="right">{product.count}</TableCell>
-              <TableCell align="right">
-                <Button
-                  size="small"
-                  color="primary"
-                  component={Link}
-                  to={`/admin/product-form/${product.id}`}
-                >
-                  Manage
-                </Button>
-                <Button
-                  size="small"
-                  color="secondary"
-                  onClick={() => onDelete(product.id)}
-                >
-                  Delete
-                </Button>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
+	return (
+		<TableContainer component={Paper}>
+			<Table aria-label="simple table">
+				<TableHead>
+					<TableRow>
+						<TableCell>Title</TableCell>
+						<TableCell align="right">Description</TableCell>
+						<TableCell align="right">Price</TableCell>
+						<TableCell align="right">Count</TableCell>
+						<TableCell align="right">Action</TableCell>
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{products.map((product: any) => (
+						<TableRow key={product.id}>
+							<TableCell component="th" scope="row">
+								{product.title}
+							</TableCell>
+							<TableCell align="right">{product.description}</TableCell>
+							<TableCell align="right">
+								{formatAsPrice(product.price)}
+							</TableCell>
+							<TableCell align="right">{product.count}</TableCell>
+							<TableCell align="right">
+								<Button
+									size="small"
+									color="primary"
+									component={Link}
+									to={`/admin/product-form/${product.id}`}
+								>
+									Manage
+								</Button>
+								<Button
+									size="small"
+									color="secondary"
+									onClick={() => onDelete(product.id)}
+								>
+									Delete
+								</Button>
+							</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
+		</TableContainer>
+	);
 }
