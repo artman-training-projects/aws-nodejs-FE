c6bd07ed426ef4a7a147703922a83b474ee83d34
ArtMan-8 <ya.Artriches@ya.ru>
2022-02-13 14:29:55 +0300

    update: api path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
 	order: "https://3qbxi9uzmg.execute-api.eu-west-1.amazonaws.com/dev",
 	import: "https://oe2jxm5cce.execute-api.eu-west-1.amazonaws.com/dev",
 	bff: "https://3qbxi9uzmg.execute-api.eu-west-1.amazonaws.com/dev",
-	cart: "https://3qbxi9uzmg.execute-api.eu-west-1.amazonaws.com/dev",
+	cart: "https://1gof246f41.execute-api.eu-west-1.amazonaws.com/dev/api",
 };
 
 export default API_PATHS;
